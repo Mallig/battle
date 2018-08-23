0a4eb0350b2fc853d3d547ad7b0686f8537ba7d9
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-08-23 14:21:14 +0100

    refactors and adds hit point display

--- app.rb
+++ app.rb
@@ -15,8 +15,6 @@ class Battle < Sinatra::Base
   end
 
   get "/play" do
-    @player_1 = session["player_1"]
-    @player_2 = session["player_2"]
     erb(:play)
   end
 
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -10,4 +10,12 @@ feature "The Battle app" do
     expect(page).to have_content("Hamish v. Sarah, the showdown of the century")
   end
 
+  scenario "can display a players hit points" do
+    visit("/")
+    fill_in("player_1", with: "Hamish")
+    fill_in("player_2", with: "Sarah")
+    click_button("Fight!")
+    expect(page).to have_content("Hamish HP: 100")
+  end
+
 end
